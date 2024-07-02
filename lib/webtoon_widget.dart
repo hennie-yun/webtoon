0f22f2a7f33ec2b36068c67b20a565b7f749c46c
hennie <hennie9401@gmail.com>
2024-07-02 17:18:03 +0900

    overflow 수정

--- lib/webtoon_widget.dart
+++ lib/webtoon_widget.dart
@@ -13,27 +13,31 @@ class Webtoon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
-            child: Container(
-              height: 100,
-              child: Image.network(
-                thumb,
-              ),
-            )),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 13,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+    Expanded(
+           child : Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+              child: Container(
+                height: 100,
+                child: Image.network(
+                  thumb,
+                ),
+              ))),
+          const SizedBox(
+            height: 10,
           ),
-        ),
-      ],
-    );
+          Text(
+            title,
+            style: const TextStyle(
+                fontSize: 13,
+                overflow: TextOverflow.ellipsis
+
+            ),
+          ),
+        ],
+
+    ) ;
   }
 }
